e2ad769296ef5c8b8395fe64cb0c32ed22d46d68
Lokut <luke.ostermann@gmail.com>
2025-05-05 15:21:26 +0200

    🔇 Remove debug logs

--- src/validators/date/is-valid-date-string.validator.ts
+++ src/validators/date/is-valid-date-string.validator.ts
@@ -8,7 +8,6 @@ import { DateTime } from 'luxon';
 @ValidatorConstraint({ name: 'isValidDateString', async: false })
 export class IsValidDateString implements ValidatorConstraintInterface {
   validate(value: string): boolean {
-    console.log('date value', (value as any) instanceof Date);
     const regex = /^\d{4}-\d{2}-\d{2}$/;
     if (!regex.test(value)) return false;
 
