065f4628c969871a8f556d32386e263d23d98637
Lokut <luke.ostermann@gmail.com>
2025-05-15 07:57:51 +0200

    📝 fix(login): Add error message on sign up

--- src/dto/auth/sign-up.dto.ts
+++ src/dto/auth/sign-up.dto.ts
@@ -5,6 +5,7 @@ import {
   IsNotEmpty,
   IsString,
   IsStrongPassword,
+  Matches,
 } from 'class-validator';
 
 export class SignUpDto {
@@ -15,6 +16,10 @@ export class SignUpDto {
   @ApiProperty({ required: true, example: 'john.doe' })
   @IsString()
   @IsNotEmpty()
+  @Matches(/^[a-zA-Z0-9._-]{3,}$/, {
+    message:
+      'Username must be at least 3 characters long and contain only letters, numbers, dots, dashes, and underscores.',
+  })
   @Transform(({ value }) => value.trim())
   username: string;
 
