120c5243e682eab4437fb64da1a76b0cdcaf92c5
Lokut <luke.ostermann@gmail.com>
2025-05-27 12:54:45 +0200

    ✨ feat(user-settings): Insert default user settings on init

--- src/modules/users/user-settings/user-settings.module.ts
+++ src/modules/users/user-settings/user-settings.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserSetting } from 'src/entities/user/setting/user-setting.entity';
+import { User } from 'src/entities/user/user.entity';
 
 import { UserSettingsController } from './user-settings.controller';
 import { UserSettingsService } from './user-settings.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserSetting])],
+  imports: [TypeOrmModule.forFeature([UserSetting, User])],
   exports: [UserSettingsService],
   providers: [UserSettingsService],
   controllers: [UserSettingsController],
--- src/modules/users/user-settings/user-settings.service.ts
+++ src/modules/users/user-settings/user-settings.service.ts
@@ -1,14 +1,67 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserSetting } from 'src/entities/user/setting/user-setting.entity';
-import { Repository } from 'typeorm';
+import { User } from 'src/entities/user/user.entity';
+import { DeepPartial, Repository } from 'typeorm';
+
+import { UserSettingTypes } from './user-setting-type.enum';
 
 @Injectable()
-export class UserSettingsService {
+export class UserSettingsService implements OnModuleInit {
+  private readonly logger = new Logger(UserSettingsService.name);
+
+  private readonly settings: DeepPartial<
+    UserSetting & { defaultValue: UserSetting['value'] }
+  >[] = [
+    {
+      code: 'TIME_ZONE',
+      defaultValue: 'Europe/Paris',
+      type: UserSettingTypes.Text,
+    },
+  ];
+
   constructor(
+    @InjectRepository(User)
+    private readonly usersRepo: Repository<User>,
     @InjectRepository(UserSetting)
     private readonly userSettingsRepo: Repository<UserSetting>,
   ) {}
+
+  async onModuleInit() {
+    for (const [_index, setting] of this.settings.entries()) {
+      const query = this.usersRepo.createQueryBuilder('user');
+
+      query.where(
+        'user.id NOT IN (SELECT user_id FROM "user_settings" "setting" WHERE "setting"."code" = :code)',
+        { code: setting.code },
+      );
+
+      query.select(['user.id', 'user.username']);
+
+      const users = (await query.getMany()) as {
+        id: number;
+        username: string;
+      }[];
+
+      const newUsersSettings: UserSetting[] = [];
+      for (const user of users) {
+        this.logger.log(
+          `Creating setting ${setting.code} for user ${user.username}`,
+        );
+
+        newUsersSettings.push(
+          this.userSettingsRepo.create({
+            code: setting.code,
+            value: setting.defaultValue,
+            type: setting.type,
+            user: { id: user.id },
+          }),
+        );
+      }
+
+      await this.userSettingsRepo.save(newUsersSettings);
+    }
+  }
 
   // #region READ
 
